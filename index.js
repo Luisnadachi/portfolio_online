013c922ca5cbab589f840c894108f4214f3ad54a
Luis Nadachi <luiseduardort2001@gmail.com>
2020-05-04 03:10:59 -0300

    Adicionado efeito no header

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-
-    let topo = document.getElementById("cima");
-
-    if (document.body.scrollTop >100 || document.documentElement.scrollTop > 100 ) {
-
-        topo.style.background = rgba(0,0,0,.8);
-        
-     } else {
-        topo.style.backgroundColor = "red";
+$(document).on('scroll',function(){
+  if ($(document).scrollTop()){
+    $("nav").removeClass("semcima");
+    $("nav").addClass("cima");
   }
-}
+else
+  {
+    $("nav").removeClass("cima");
+    $("nav").addClass("semcima");
+  }
+})
